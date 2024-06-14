ba59421e7021cbb5f322aae965bf46515155e12b
ricirici <bajraktarevicrijalda@gmail.com>
2024-06-14 10:53:18 +0200

    Enabled to get CSV with campaign result a month after end of the campaign

--- Loyalty campaigns/Loyalty campaigns/APIs/Controllers/PurchaseController.cs
+++ Loyalty campaigns/Loyalty campaigns/APIs/Controllers/PurchaseController.cs
@@ -16,12 +16,26 @@ namespace Loyalty_campaigns.APIs.Controllers
         [HttpGet("export_csv")]
         public async Task<IActionResult> ExportProductsToCsv()
         {
-            var csvFilePath =  _purchaseService.GetSuccessfulPurchasesCSVAsync();
+            //DateTime endDateOfCampaign = new DateTime(2024, 7, 1);
+            DateTime endDateOfCampaign = new DateTime(2024, 6, 13);//for test purpose
+            DateTime currentDate = DateTime.UtcNow.Date;
 
-            var csvBytes = await System.IO.File.ReadAllBytesAsync(csvFilePath);
-            var csvFileName = "successful_purchases.csv";
+            //if (currentDate >= endDateOfCampaign.AddMonths(1))
+            if (currentDate >= endDateOfCampaign.AddDays(1)) //for test purpose
+            {
+                var csvFilePath = _purchaseService.GetSuccessfulPurchasesCSVAsync();
 
-            return File(csvBytes, "text/csv", csvFileName);
+                var csvBytes = await System.IO.File.ReadAllBytesAsync(csvFilePath);
+                var csvFileName = "successful_purchases.csv";
+
+                return File(csvBytes, "text/csv", csvFileName);
+            }
+            else
+            {
+               
+                return StatusCode(403, "Results not available yet.");
+            }
+            
         }
     }
 }
